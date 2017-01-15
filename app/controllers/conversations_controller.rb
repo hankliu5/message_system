842e409b8af09b885cd5a1b80d36da6c33a72c8a
hankliu5 <liu19901124@gmail.com>
2017-01-14 21:31:56 -0800

    finished websocket

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -4,10 +4,15 @@ class ConversationsController < ApplicationController
 
   def index
     @conversations = Conversation.participating(current_user).order('updated_at DESC')
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def show
-  @personal_message = PersonalMessage.new
+    @conversation = Conversation.find_by(id: params[:id])
+    @personal_message = PersonalMessage.new
   end
 
   private
