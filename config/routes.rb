ccfe833a88d7d493ba8a2a58df560d9c1e2b4bd4
hankliu5 <liu19901124@gmail.com>
2017-01-14 21:11:28 -0800

    finished the implementation of basic framework. ready for activecable.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index]
+  resources :personal_messages, only: [:new, :create]
+  resources :conversations, only: [:index, :show]
+
+  mount ActionCable.server => '/cable'
+
+  root 'conversations#index'
 end
